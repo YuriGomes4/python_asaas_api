d2b7db24a8f86fac594a377df3384784fc6b0dc5
Yuri Gomes <yurialdegomes@hotmail.com>
2024-02-16 01:05:12 -0300

    ver assinatura

--- py_asaas/wrapper.py
+++ py_asaas/wrapper.py
@@ -166,3 +166,32 @@ class assinatura(auth):
         
         else:
             return {}
+        
+    def ver_assinatura(self, id_assinatura):
+        """
+        Descrição da função
+        """
+        #Descrição da função
+
+        asct = True #Acesso Só Com Token
+
+        if asct and (self.access_token == "" or self.access_token == None or type(self.access_token) != str):
+            print("Token inválido")
+            return {}
+
+        url = self.base_url+f"/v3/subscriptions/{id_assinatura}"
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'access_token': self.access_token
+        }
+
+        response = self.request("GET", url=url, headers=headers)
+
+        if response:
+
+            return response.json()
+        
+        else:
+            return {}
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as arq:
     readme = arq.read()
 
 setup(name='py_asaas',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT License',
     author='Yuri Gomes',
     long_description=readme,
